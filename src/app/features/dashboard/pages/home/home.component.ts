371156470fa0e20823f53cb2fb26f1f5095b8e6b
Shaimaa Siraj Ezzat Khalil <shaimaakhalil560@gmail.com>
2025-02-01 21:04:05 +0200

    navbar design, carsoul poc

--- src/app/features/dashboard/pages/home/home.component.ts
+++ src/app/features/dashboard/pages/home/home.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild  } from '@angular/core';
+import { SHARED_IMPORTS } from '../../../../shared/shared-imports';
+import { NgbCarousel, NgbCarouselModule, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [...SHARED_IMPORTS,NgbCarouselModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  images = [62, 83, 466, 965, 982, 1043, 738].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
+	paused = false;
+	unpauseOnArrow = false;
+	pauseOnIndicator = false;
+	pauseOnHover = true;
+	pauseOnFocus = true;
+
+	@ViewChild('carousel', { static: true }) carousel!: NgbCarousel;
+
+	togglePaused() {
+		if (this.paused) {
+			this.carousel.cycle();
+		} else {
+			this.carousel.pause();
+		}
+		this.paused = !this.paused;
+	}
+
+	onSlide(slideEvent: NgbSlideEvent) {
+		if (
+			this.unpauseOnArrow &&
+			slideEvent.paused &&
+			(slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
+		) {
+			this.togglePaused();
+		}
+		if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+			this.togglePaused();
+		}
+	}
 }
